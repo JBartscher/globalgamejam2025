8b898a11fd83aa158354aa175d174361f517b63f
jasper <jasper.bartscher@gmail.com>
2025-01-25 02:37:33 +0100

    Add curve generation

--- src/ship.rs
+++ src/ship.rs
@@ -8,15 +8,15 @@ pub struct ShipPlugin;
 
 #[derive(AssetCollection, Resource)]
 pub struct ShipAssets {
-    #[asset(path = "3d/ships/boat-sail-a.glb")]
+    #[asset(path = "3d/ships/boat-sail-a.glb#Scene0")]
     sail_a: Handle<Scene>,
-    #[asset(path = "3d/ships/boat-sail-a.glb")]
+    #[asset(path = "3d/ships/boat-sail-b.glb#Scene0")]
     sail_b: Handle<Scene>,
-    #[asset(path = "3d/ships/ship-cargo-a.glb")]
+    #[asset(path = "3d/ships/ship-cargo-a.glb#Scene0")]
     cargo_a: Handle<Scene>,
-    #[asset(path = "3d/ships/ship-cargo-b.glb")]
+    #[asset(path = "3d/ships/ship-cargo-b.glb#Scene0")]
     cargo_b: Handle<Scene>,
-    #[asset(path = "3d/ships/ship-cargo-c.glb")]
+    #[asset(path = "3d/ships/ship-cargo-c.glb#Scene0")]
     cargo_c: Handle<Scene>,
 }
 
@@ -31,6 +31,12 @@ struct Ship {
     front: Vec3,
     back_left: Vec3,
     back_right: Vec3,
+}
+
+#[derive(Component)]
+struct PathFollow {
+    pub curve: CubicCurve<Vec3>,
+    t: f32
 }
 
 impl Ship {
@@ -65,18 +71,50 @@ fn spawn_ship(
     ship_assets: Res<ShipAssets>,
     asset_server: Res<AssetServer>,
 ) {
-    let mut transform = Transform::from_xyz(0.0, 0.5, 0.0);
-    transform.scale = Vec3::new(2., 2., -2.);
+    let mut transform_1 = Transform::from_xyz(0.0, 0.0, 0.0);
+    transform_1.scale = Vec3::new(2., 2., 2.);
 
+    let control_points = vec![
+        Vec3::new(0.0, 0.0, 0.0),
+        Vec3::new(125.0, 0.0, 0.0),
+        Vec3::new(0.0, 0.0, -125.0),
+        Vec3::new(-125.0, 0.0, 0.0),
+    ];
+    // Create the CubicBSpline -> CubicCurve<Vec3>
+    let curve = CubicCardinalSpline::new_catmull_rom(control_points).to_curve_cyclic().unwrap();
+
+    // sail a
     commands.spawn((
         SceneRoot(
             // GltfAssetLabel::Scene(0).clone_from(ship_assets.sail_a.clone()),
-            // ship_assets.sail_b.clone().into(),
-            asset_server.load(GltfAssetLabel::Scene(0).from_asset("3d/ships/boat-sail-b.glb")),
+            ship_assets.sail_a.clone().into(),
+            // asset_server.load(GltfAssetLabel::Scene(0).from_asset("3d/ships/boat-sail-b.glb")),
         ),
         Ship::new(-0.100, -3., 3.0, -2.0, 2.0),
-        transform,
+        transform_1,
+        PathFollow{ curve, t: 0.0 }
     ));
+    
+}
+
+/// This system uses gizmos to draw the current [`Curve`] by breaking it up into a large number
+/// of line segments.
+fn draw_follow_path(path_follow_query: Query<&PathFollow>, mut gizmos: Gizmos) {
+
+    for (p) in &path_follow_query {
+        // Scale resolution with curve length so it doesn't degrade as the length increases.
+       //  let resolution = 100 * p.curve. .len();
+        gizmos.linestrip(
+            p.curve.iter_positions(100),
+            Color::srgb(1.0, 0.5, 0.2),
+        );
+    }
+
+}
+
+
+pub fn move_ship(mut ships: Query<(&Ship, &mut Transform)>){
+
 }
 
 pub fn update_ships(
@@ -89,17 +127,17 @@ pub fn update_ships(
         ship.update(&water, pos, &mut transform);
 
         // front
-        gizmos.arrow(ship.front, ship.front + Vec3::new(0., 10., 0.), YELLOW);
+        gizmos.arrow(ship.front + transform.translation, ship.front + transform.translation + Vec3::new(0., 10., 0.), YELLOW);
         // back left
         gizmos.arrow(
-            ship.back_left,
-            ship.back_left + Vec3::new(0., 10., 0.),
+            ship.back_left + transform.translation,
+            ship.back_left + transform.translation + Vec3::new(0., 10., 0.),
             YELLOW,
         );
         // back right
         gizmos.arrow(
-            ship.back_right,
-            ship.back_right + Vec3::new(0., 10., 0.),
+            ship.back_right + transform.translation,
+            ship.back_right + transform.translation + Vec3::new(0., 10., 0.),
             YELLOW,
         );
     }
@@ -109,6 +147,8 @@ impl Plugin for ShipPlugin {
     fn build(&self, app: &mut App) {
         app
             .add_systems(OnEnter(GameState::Game), spawn_ship)
-            .add_systems(Update, update_ships.run_if(in_state(GameState::Game)));
+            .add_systems(Update, update_ships.run_if(in_state(GameState::Game)))
+            .add_systems(Update, draw_follow_path.run_if(in_state(GameState::Game)))
+        ;
     }
 }
